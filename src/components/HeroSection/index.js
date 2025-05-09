59abe07d08704b5da8a2856559b56895aa585591
farzana84 <U2450510@rts-365.dk>
2025-05-09 12:16:28 +0200

    first commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+//import { Bio } from "../data/constants";
+import HeroImg from "../../images/FarzanaZaman.jpg";
+import HeroBgAnimation from "../HeroBgAnimation";
+import {
+  HeroBg,
+  HeroContainer,
+  HeroInnerContainer,
+  HeroLeftContainer,
+  HeroRightContainer,
+  Img,
+  ResumeButton,
+  Span,
+  SubTitle,
+  TextLoop,
+  Title,
+} from "./HeroStyle";
+import { Bio } from "../data/constants";
+
+const HeroSection = () => {
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <SubTitle>{Bio.description2}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Img src={HeroImg} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
+
+export default HeroSection;
